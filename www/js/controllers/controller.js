1de44dd577a5ba9cb6d1057cb32b1f048f46b7ea
kuea <k3n3th@gmail.com>
2016-03-20 17:17:04 -0600

    "testimonio y mejoras"

--- www/js/controllers/controller.js
+++ www/js/controllers/controller.js
@@ -12,26 +12,39 @@ angular.module('controller', [])
     $scope.hola = function (pancho) {
         console.log(pancho + ': ' + factory.panchoElTriste());
     }
+
+    /*
+        Carrousel Testimonios
+    */
     $scope.navbarCollapsed = true;
     $scope.myInterval = 5000;
     $scope.noWrapSlides = false;
     $scope.active = 0;
     var slides = $scope.slides = [];
     var currIndex = 0;
+    var testimonios = factory.getTestimonios();
 
-    $scope.addSlide = function () {
-        var newWidth = 600 + slides.length + 1;
+    for (var i = 0; i < testimonios.length; i++) {
         slides.push({
-            image: 'http://lorempixel.com/' + newWidth + '/300',
-            text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+            color: testimonios[i].color,
+            nombre: testimonios[i].nombre,
+            foto: testimonios[i].foto,
+            cuerpo: testimonios[i].cuerpo,
             id: currIndex++
         });
-    };
-
-
-    for (var i = 0; i < 4; i++) {
-        $scope.addSlide();
     }
+    /*
+        Carrousel Testimonios
+    */
+
+    /*
+        Clientes
+    */
+    $scope.clientes = factory.getClientes();
+    /*
+    Clientes
+    */
+
     /*------------------------ Carrousel Productos --------------------*/
     $scope.navbarCollapsedP = true;
     $scope.myIntervalP = 5000;
--- www/js/factories/factory.js
+++ www/js/factories/factory.js
@@ -29,12 +29,73 @@ angular.module('factories', [])
         }
     ];
 
-    comun.panchoElTriste = function(){
+    var clientes = [
+        {
+            nombre: 'Miel Estrada',
+            logo: 'http://previews.123rf.com/images/didora/didora1409/didora140900543/32021099-abeja-de-la-miel-y-el-panal-de-miel-icono-de-dise-o-plano-Foto-de-archivo.jpg'
+    }, {
+            nombre: 'Facebook',
+            logo: 'http://previews.123rf.com/images/didora/didora1409/didora140900543/32021099-abeja-de-la-miel-y-el-panal-de-miel-icono-de-dise-o-plano-Foto-de-archivo.jpg'
+    }, {
+            nombre: 'google',
+            logo: 'http://previews.123rf.com/images/didora/didora1409/didora140900543/32021099-abeja-de-la-miel-y-el-panal-de-miel-icono-de-dise-o-plano-Foto-de-archivo.jpg'
+    }, {
+            nombre: 'twiter',
+            logo: 'http://previews.123rf.com/images/didora/didora1409/didora140900543/32021099-abeja-de-la-miel-y-el-panal-de-miel-icono-de-dise-o-plano-Foto-de-archivo.jpg'
+    }, {
+            nombre: 'Chaplist',
+            logo: 'http://previews.123rf.com/images/didora/didora1409/didora140900543/32021099-abeja-de-la-miel-y-el-panal-de-miel-icono-de-dise-o-plano-Foto-de-archivo.jpg'
+    }
+                   ];
+
+    var testimonios = [
+        {
+            color: '#E1BEE7',
+            nombre: 'Jose Estrada',
+            foto: 'https://s3.amazonaws.com/uifaces/faces/twitter/mantia/128.jpg',
+            cuerpo: 'Muy Buena experiencia, es exelente, adios mucho mgusto',
+            id: ''
+    }, {
+            color: '##B2DFDB',
+            nombre: 'TAmy Vivas',
+            foto: 'https://s3.amazonaws.com/uifaces/faces/twitter/mantia/128.jpg',
+            cuerpo: 'Muy Buena experiencia, me parecen exelentes apps',
+            id: ''
+    }, {
+            color: '#BBDEFB',
+            nombre: 'Jeyson Montenegro',
+            foto: 'https://s3.amazonaws.com/uifaces/faces/twitter/mantia/128.jpg',
+            cuerpo: 'Muy Buen trabajo departe de los muchachos!',
+            id: ''
+    }, {
+            color: '#F0F4C3',
+            nombre: 'Jeremias Ortega',
+            foto: 'https://s3.amazonaws.com/uifaces/faces/twitter/mantia/128.jpg',
+            cuerpo: 'Exelente!!!!!!!',
+            id: ''
+    }, {
+            color: '#FFE0B2',
+            nombre: 'Francisco Ardon',
+            foto: 'https://s3.amazonaws.com/uifaces/faces/twitter/mantia/128.jpg',
+            cuerpo: 'Generando buenas ideas!!!!',
+            id: ''
+    }
+                      ];
+
+    comun.panchoElTriste = function () {
         return 'estoy triste';
     }
 
-    comun.getMembers = function(){
+    comun.getMembers = function () {
         return array;
+    }
+
+    comun.getClientes = function () {
+        return clientes;
+    }
+
+    comun.getTestimonios = function () {
+        return testimonios;
     }
 
     return comun
