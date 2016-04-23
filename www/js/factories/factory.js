815a486026a62c721b63a25556e3dcf77e2ea4c5
esvin <esvin.jose.estrada@gmail.com>
2016-04-23 13:06:16 -0600

    Ortografia fix, Tamanios

--- www/js/factories/factory.js
+++ www/js/factories/factory.js
@@ -44,17 +44,17 @@ angular.module('factories', [])
     var arraySkills = [
         {
             name: 'Android',
-            description: 'Desarrollamos Apps hibridas y nativas con la mas reciente tecnologia de desarrollo y diseño.',
+            description: 'Desarrollamos Apps hibridas y nativas, con la mas recientes tecnologías de desarrollo y diseño.',
             icon: 'https://cdn1.iconfinder.com/data/icons/simple-icons/4096/android-4096-black.png'
         },
         {
             name: 'SailsJS',
-            description: 'Utilizamos Frameworks para desarrollar las soluciones. Esto nos permite dar una solucion integral y optima.',
+            description: 'Utilizamos Frameworks para desarrollar las soluciones. Esto nos permite dar una solucion integral, mejorable, mantenible y optima.',
             icon: 'https://cdn.tutsplus.com/net/uploads/2013/06/sails-preview-400-2.jpg'
         },
         {
             name: 'IT',
-            description: 'Presatmos servico de la tecnologia de la informacion para mantener su empresa al dia y al ritmo del cambio tecnologico.',
+            description: 'Prestamos servicios de la tecnologia de la informacion para administrar su empresa, manteniendola a la vanguardia y al ritmo del cambio tecnologico.',
             icon: 'http://edelitconsult.com/wp-content/uploads/2013/12/it-consulting-solutions-w600h400.jpg'
         }
     ];
